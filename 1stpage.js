17bf8cf2f96b3015e6af7964165965f5b8f2348c
Sadik Shaik <sksadik45264@gmail.com>
2026-04-14 11:34:08 +0530

    Initial commit

---
+++ 1stpage.js
@@ -0,0 +1,16 @@
+function about(){
+    window.location.href = "about.html";
+}
+function contact(){
+    window.location.href = "contact.html";
+}
+
+function services(){
+    window.location.href = "services.html";
+}
+function home(){
+    window.location.href = "1stpageclone.html";
+}
+function help(){
+    window.location.href = "help.html";
+}
